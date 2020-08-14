5e3f11539e1893ccccd8e37c6ffd2d899093158b
rzounlome <rzounlome@harmonyhit.com>
2020-08-14 16:48:44 -0400

    setup navbar

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import "./Navbar.scss";
 
+import ScrollspyNav from "react-scrollspy-nav";
+
 const Navbar = () => {
   const [scroll, setScroll] = useState(false);
 
@@ -15,9 +17,35 @@ const Navbar = () => {
     });
   });
   return (
-    <div className={!scroll ? "navbar" : "navbar-scroll"} id="navbar">
-      Navbar
-    </div>
+    <nav className={!scroll ? "navbar" : "navbar-scroll"} id="navbar">
+      <div className="logo">Logo</div>
+      <ScrollspyNav
+        scrollTargetIds={["landing", "services", "about", "team", "contact"]}
+        offset={100}
+        activeNavClass="is-active"
+        scrollDuration="1000"
+        headerBackground="true"
+        className="nav"
+      >
+        <ul>
+          <li>
+            <a href="#landing">Home</a>
+          </li>
+          <li>
+            <a href="#services">Services</a>
+          </li>
+          <li>
+            <a href="#about">About</a>
+          </li>
+          <li>
+            <a href="#team">Team</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+        </ul>
+      </ScrollspyNav>
+    </nav>
   );
 };
 
