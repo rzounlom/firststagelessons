7afacf0a33daa20895b06890598b13bafe205afa
rzounlome <rzounlome@harmonyhit.com>
2020-08-14 09:52:24 -0400

    added deploy setup

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
-import express from "express";
+const express = require("express");
 const app = express();
-import cors from "cors";
+const cors = require("cors");
+const path = require("path");
 const PORT = process.env.PORT || 5000;
 
 app.use(cors());
+app.use(express.json());
 
-app.get("/", (req, res) => res.json("Homepage"));
+if (process.env.NODE_ENV === "production") {
+  //set static fuilder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 app.listen(PORT, () => console.log(`Server Started on port: ${PORT}`));
