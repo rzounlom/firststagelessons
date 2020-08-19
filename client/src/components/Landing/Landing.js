253ac87ed09e1e016499265247c28538321a3e0f
rzounlome <rzounlome@harmonyhit.com>
2020-08-19 09:31:12 -0400

    complete landing section

--- client/src/components/Landing/Landing.js
+++ client/src/components/Landing/Landing.js
@@ -2,7 +2,20 @@ import React from "react";
 import "./Landing.scss";
 
 const Landing = () => {
-  return <div className="landing" id="landing" />;
+  return (
+    <div className="landing" id="landing">
+      <div className="landing-heading">
+        <div className="landing-text">
+          <h1>First Stage Guitar Lessons</h1>
+        </div>
+        <div className="landing-btn">
+          <a href="#contact">
+            <button>Contact</button>{" "}
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Landing;
