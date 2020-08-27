e0d3ada23566ba20150ac2aa87534a7452eadd99
rzounlome <rzounlome@harmonyhit.com>
2020-08-26 23:59:19 -0400

    add rough draft of contact section; addjust team cards for mobile

--- client/src/components/Contact/Contact.js
+++ client/src/components/Contact/Contact.js
@@ -4,7 +4,13 @@ import "./Contact.scss";
 const Contact = () => {
   return (
     <div className="contact" id="contact">
-      Contact
+      <div className="contact-heading">
+        <h1>Contact</h1>
+      </div>
+      <div className="contact-content">
+        <div className="contact-picture">Contact Pic</div>
+        <div className="contact-form">Contact Form</div>
+      </div>
     </div>
   );
 };
