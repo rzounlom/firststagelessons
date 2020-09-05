32251c276922c1f4bb13d2320c9e77e8062d77f5
rzounlome <rzounlome@harmonyhit.com>
2020-09-05 11:57:02 -0400

    removed api key from index

--- index.js
+++ index.js
@@ -1,32 +1,30 @@
-//api key: "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"
 const express = require("express");
 const app = express();
 const cors = require("cors");
 const path = require("path");
+require("dotenv").config({ path: "./.env" });
 const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
-// using Twilio SendGrid's v3 Node.js Library
-// https://github.com/sendgrid/sendgrid-nodejs
 const sgMail = require("@sendgrid/mail");
-sgMail.setApiKey(
-  "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"
-);
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-app.get("/send-email", async (req, res) => {
-  // const {name, email, subject, message} = req.body
+app.post("/send-email", async (req, res) => {
+  const { name, email, subject, message } = req.body;
   const msg = {
-    to: "skillz21c@yahoo.com",
+    to: email,
     from: "rzounlom@gmail.com",
-    subject: "Sending with Twilio SendGrid is Fun",
-    text: "and easy to do anywhere, even with Node.js",
-    html: "<strong>and easy to do anywhere, even with Node.js</strong>",
+    subject: subject,
+    text: message,
+    html: `<p><strong>New message from: ${name} at email: ${email} message: </strong>${message}</p>`,
   };
 
-  await sgMail.send(msg).catch((e) => console.log(e));
-  res.json({ msg: "email sent" });
+  await sgMail
+    .send(msg)
+    .then((msg) => res.json({ status: "email sent", message: msg }))
+    .catch((e) => console.log(e));
 });
 
 if (process.env.NODE_ENV === "production") {
