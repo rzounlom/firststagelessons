0d1ecbefb97dce27d8361b2bf44ee8d5ad0cd56d
rzounlome <rzounlome@harmonyhit.com>
2020-09-05 11:39:06 -0400

    added contact form functionality to fontend and backend using sendgrid and axios

--- client/src/components/Contact/ContactForm.js
+++ client/src/components/Contact/ContactForm.js
@@ -18,14 +18,24 @@ const ContactForm = () => {
 
   const onFormSubmit = async (event) => {
     event.preventDefault();
-    await axios.get("http:localhost:5000/send-email", sendEmail);
-    console.log(sendEmail);
-    setSendEmail({
-      name: "",
-      email: "",
-      subject: "",
-      message: "",
-    });
+    try {
+      console.log(sendEmail);
+      await axios.post("http://localhost:5000/send-email", sendEmail);
+      setSendEmail({
+        name: "",
+        email: "",
+        subject: "",
+        message: "",
+      });
+    } catch (error) {
+      console.error(error);
+      setSendEmail({
+        name: "",
+        email: "",
+        subject: "",
+        message: "",
+      });
+    }
   };
 
   return (
