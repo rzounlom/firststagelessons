3a202108a223143b8df9f35b485c9bb08d7295df
rzounlome <rzounlome@harmonyhit.com>
2020-09-07 21:30:33 -0400

    setup payment backend; changed mail api key

--- client/src/components/StripeCheckout/StripeCheckoutButton/StripeCheckoutButton.jsx
+++ client/src/components/StripeCheckout/StripeCheckoutButton/StripeCheckoutButton.jsx
@@ -1,13 +1,31 @@
 import React from "react";
 import StripeCheckout from "react-stripe-checkout";
+import axios from "axios";
+
+const PORT = process.env.PORT || 5000;
 
 const StripeCheckoutButton = ({ price, name }) => {
   const priceForStripe = price * 100;
   const publishableKey = "pk_test_UP1rx1dUY6Pr1CRG0BMRg80t00L2Zu1igZ";
 
   const onToken = (token) => {
-    console.log(token);
-    alert("Payment Successful");
+    axios({
+      url: `http://localhost:${PORT}/payment`,
+      method: "post",
+      data: {
+        amount: priceForStripe,
+        token,
+      },
+    })
+      .then((response) => {
+        alert("Payment Successful");
+      })
+      .catch((error) => {
+        console.log(error);
+        alert(
+          "There was an issue with you payment. Please check your card info and try again"
+        );
+      });
   };
   return (
     <StripeCheckout
--- index.js
+++ index.js
@@ -1,21 +1,24 @@
 const express = require("express");
-const app = express();
 const cors = require("cors");
 const path = require("path");
+const bodyParser = require("body-parser");
+const sgMail = require("@sendgrid/mail");
 require("dotenv").config({ path: "./.env" });
 const PORT = process.env.PORT || 5000;
+
+const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-const sgMail = require("@sendgrid/mail");
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 app.post("/send-email", async (req, res) => {
   const { name, email, subject, message } = req.body;
   const msg = {
     to: email,
-    from: "rzounlom@gmail.com",
+    from: "firstshiftllc@gmail.com",
     subject: subject,
     text: message,
     html: `<p><strong>New message from: ${name} at email: ${email} message: </strong>${message}</p>`,
@@ -37,3 +40,20 @@ if (process.env.NODE_ENV === "production") {
 }
 
 app.listen(PORT, () => console.log(`Server Started on port: ${PORT}`));
+
+app.post("/payment", (req, res) => {
+  const body = {
+    source: req.body.token.id,
+    amount: req.body.amount,
+    currency: "usd",
+  };
+
+  stripe.charges.create(body, (stripeErr, stripeRes) => {
+    if (stripeErr) {
+      res.status(500).send({ error: stripeErr });
+      console.log(stripeErr);
+    } else {
+      res.status(200).send({ success: stripeRes });
+    }
+  });
+});
