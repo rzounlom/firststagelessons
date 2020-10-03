930ac9a02a38b97e041c21b3eabe0e466e6918f7
rzounlom <rzounlom@gmail.com>
2020-10-03 19:06:15 -0400

    updated index

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-const express = require("express");
-const cors = require("cors");
-const path = require("path");
-const bodyParser = require("body-parser");
-const sgMail = require("@sendgrid/mail");
-require("dotenv").config({ path: "./.env" });
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+const bodyParser = require('body-parser');
+const sgMail = require('@sendgrid/mail');
+require('dotenv').config({ path: './.env' });
 const PORT = process.env.PORT || 5000;
-
+//need
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
-app.post("/send-email", async (req, res) => {
+app.post('/send-email', async (req, res) => {
   const { name, email, subject, message } = req.body;
   const msg = {
     to: email,
-    from: "firstshiftllc@gmail.com",
+    from: 'firstshiftllc@gmail.com',
     subject: subject,
     text: message,
     html: `<p><strong>New message from: ${name} at email: ${email} message: </strong>${message}</p>`,
@@ -26,24 +26,24 @@ app.post("/send-email", async (req, res) => {
 
   await sgMail
     .send(msg)
-    .then((msg) => res.json({ status: "email sent", message: msg }))
+    .then((msg) => res.json({ status: 'email sent', message: msg }))
     .catch((e) => console.log(e));
 });
 
-if (process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV === 'production') {
   //set static fuilder
-  app.use(express.static("client/build"));
+  app.use(express.static('client/build'));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
 
-app.post("/payment", (req, res) => {
+app.post('/payment', (req, res) => {
   const body = {
     source: req.body.token.id,
     amount: req.body.amount,
-    currency: "usd",
+    currency: 'usd',
   };
 
   stripe.charges.create(body, (stripeErr, stripeRes) => {
